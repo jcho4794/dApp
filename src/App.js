55a24eef1fc09cd74437790320cfaa306200adee
Nithin Raj <nithin.raj@iiitb.org>
2018-08-25 17:53:00 +0530

    fixed invalid routing error
    Added a route to handle invalid exchange routes
    
    removed Switch component import from react-router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Layout } from 'antd';
 import React, { Component } from 'react';
-import { Route, Router, Redirect, Switch } from 'react-router';
+import { Route, Router, Redirect } from 'react-router';
 
 import Header from './components/Header';
 import MarketFooter from './components/MarketFooter';
@@ -21,18 +21,16 @@ class App extends Component {
   render() {
     return (
       <Router history={this.props.history}>
-        <Switch>
           <Layout style={{ minHeight: '100vh' }}>
             <Header />
-            <Content>
-              {routes.map(route => <Route key={route.path} {...route} />)}
-              <Redirect from='/exchange/*' to='/exchange' />
-            </Content>
-
+              <Content>
+                {routes.map(route => <Route key={route.path} {...route} />)}
+                <Route path='/exchange/*' render={()=> ( 
+                  <Redirect to='/exchange' />
+                )}/>
+              </Content>
             <MarketFooter />
           </Layout>
-          
-        </Switch>
       </Router>
     );
   }
